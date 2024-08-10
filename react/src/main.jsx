b7aff96ce5dfef87300ed394445518c9425fa153
Sajid Ali <mhsajidalikhan@gmail.com>
2024-08-10 12:40:05 +0500

    added route files, integrated the react router.

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Outlet } from 'react-router-dom'
+import { ContextProvider } from './contextporvider/ContextProvider.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <Outlet />
+    </ContextProvider>
+
   </React.StrictMode>,
 )
--- react/src/router.jsx
+++ react/src/router.jsx
@@ -3,26 +3,52 @@ import Login from './views/Login.jsx';
 import User from './views/User';
 import Signup from './views/Signup';
 import Notfound from './Notfound';
+import GuestLayout from './components/GuestLayout.jsx';
+import DefaultLayout from './components/DefaultLayout.jsx';
+import { Children } from 'react';
+import Dashboard from './views/Dashboard.jsx';
 
 
 
 const router = createBrowserRouter([
+
     {
-        path: 'login',
-        element: <Login />
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+
+            {
+                path: 'login',
+                element: <Login />
+            },
+            {
+                path: 'signup',
+                element: <Signup />
+            },
+            {
+                path: '',
+                element: <Notfound />
+            }
+        ]
     },
+
     {
-        path: 'users',
-        element: <User />
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: 'dashboard',
+                element: <Dashboard />
+            },
+            {
+                path: 'users',
+                element: <User />
+            }
+
+        ]
     },
-    {
-        path: 'signup',
-        element: <Signup />
-    },
-    {
-        path : '',
-        element: <Notfound />
-    }
+  
+
 
 ])
 
