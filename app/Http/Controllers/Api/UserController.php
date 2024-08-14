09f08abe357bc7ae41073fe201ef5e77c92c9d6b
Sajid Ali <mhsajidalikhan@gmail.com>
2024-08-14 12:58:35 +0500

    added resourse definiton

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use GuzzleHttp\Promise\Create;
 
 class UserController extends Controller
 {
@@ -16,7 +18,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return  UserResource::collection(
+            User::query()->orderBy('id', 'desc')->paginate()
+        );
     }
 
     /**
@@ -28,6 +32,10 @@ class UserController extends Controller
     public function store(StoreUserRequest $request)
     {
         //
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+        return response(new UserResource($user), 201);
     }
 
     /**
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,11 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s')
+        ]
     }
 }
